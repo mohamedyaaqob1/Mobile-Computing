361fc5e22cad6cab311b7912efd40213114a0cd1
mohamedyaaqob1 <yaaqobmohamed@gmail.com>
2023-03-25 17:52:52 +0200

    description

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text("First Application"),
+      ),
+      body: Center(
+        child: Text(
+          "Mohamed sayed mohamed yaaqob",
+        
+        ),
+      ),
+    );
+  }
+}
